d27a6ac53c5bdced94c009e4c816054a406b3ca5
Facundo Rubin <facundorubin@gmail.com>
2015-06-06 13:15:00 -0300

    agregue get de inscriptos

--- administracion/certificados_view.php
+++ administracion/certificados_view.php
@@ -1,37 +1,49 @@
 <!DOCTYPE HTML>
-<html lang="es"  ng-app="Acreditacion">
+<html lang="es"  ng-app="acreditacion">
 	<head>
+		<meta charset="utf-8" />
+		<style>
+			
+			.tabla_inscriptos tr td{
+				border-bottom: 1px solid grey;
+				font-size: 0.9em;
+				background-color: white;
+				color: black;
+				margin: 0px;
+			}
+			.tabla_inscriptos th{
+				font-weight: bolder;
+				font-size: 1em;
+			}
+			.center{
+				text-align: center;
+				margin:auto;
+			}
+		</style>
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 		<script src="js/angular.min.js"></script>
-		<!--<script src="js/script.js"></script>-->
+		<script src="js/app.js"></script>
 		<title>Certificados</title>
 	</head>
 	 <body> 
 		<header>
 			<img src="images/teyet.png">
 		</header>
-		<?php include_once("includes/menu.php"); ?>
-		<?php 
-			require_once("../config.php");
-			$conexion = mysqli_connect($config_servidor_bd,$config_usuario,$config_clave,$config_bd_name);
-			if ($conexion->connect_errno) {
-				echo "Fallo al contenctar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
-			die;
-			}
-
-			$consulta = "select per.ayn, per.correo from inscripciones as insc left join personas as per on per.id_persona = insc.id_persona";
-
-			$resultado = $conexion->query($consulta);
-			
-		?>
-		<table class="tabla_inscriptos center">
-			<th>Apellido y Nombre</th><th>Accion</th>
-			<?php while ($registro = $resultado->fetch_object()) : ?>
-			<tr>
-			<td><?php echo $registro->ayn; ?></td>
-			<td><a target="_BLANK" href="../procesos/crear/certificado_asistencia.php?nombre=<?php echo strtolower(str_replace(array(' ','/'),'_',$registro->ayn)); ?>&correo=<?php echo $registro->correo; ?>" >Ver</a></td>
-			</tr>
-			<?php endwhile; ?>
-		</table>
 		
+		<div ng-controller="AcreditacionCtr as acreCtrl">
+			<input type="text" ng-model="acreCtrl.filtro" />
+			<table class="tabla_inscriptos center">
+				<th>Apellido y Nombre</th><th>Accion</th>
+				<tr ng-repeat="insc in acreCtrl.inscriptos | filter:acreCtrl.filtro">
+				<td>{{insc.ayn}} ({{insc.email}})</td>
+				<td><a target="_BLANK" href="../procesos/crear.php?nombre={{insc.ayn}}&correo={{insc.email}}">Ver</a></td>
+				<!-- <td ng-if="!acreCtrl.coincidencia(insc.ayn)">{{insc.ayn}} ({{insc.email}})</td>
+				<td ng-if="!acreCtrl.coincidencia(insc.ayn)"><a target="_BLANK" href="../procesos/crear.php?nombre=<?php echo strtolower(str_replace(array(' ','/'),'_',$registro->ayn)); ?>&correo=<?php echo $registro->correo; ?>" >Ver</a></td> -->
+				</tr>
+				
+			</table>
+		</div>
+		<script type="text/javascript">
+		
+		</script>
 </body>
--- administracion/js/app.js
+++ administracion/js/app.js
@@ -5,56 +5,64 @@ var app = angular.module('acreditacion', []);
 		
 // 	});
 
-app.controller('AcreditacionCtr',function()
+app.controller('AcreditacionCtr',function($scope)
 	{
-		this.filtro;
-		this.inscriptos = [
-		{
-			ayn: "Marangolo Alessandra Luana",
-			email:"ale.marangolo@gmail.com"
+		$scope.filtro;
+		// this.inscriptos = [
+		// {
+		// 	ayn: "Marangolo Alessandra Luana",
+		// 	email:"ale.marangolo@gmail.com"
 
-		},
-		{
-			ayn: "Alonso Alejandra",
-			email:"alto02@hotmail.com"
+		// },
+		// {
+		// 	ayn: "Alonso Alejandra",
+		// 	email:"alto02@hotmail.com"
 
-		},
-		{
-			ayn: "Sanchez Alvarez Milagros Samanta",
-			email:"alvarezmilagros@gmail.com"
+		// },
+		// {
+		// 	ayn: "Sanchez Alvarez Milagros Samanta",
+		// 	email:"alvarezmilagros@gmail.com"
 
-		},
-		{
-			ayn: "Belcastro Ángela",
-			email:"angelab@ing.unp.edu.ar"
+		// },
+		// {
+		// 	ayn: "Belcastro Ángela",
+		// 	email:"angelab@ing.unp.edu.ar"
 
-		},
-		{
-			ayn: "Silva Augusto",
-			email:"augustosilva5949@hotmail.com"
+		// },
+		// {
+		// 	ayn: "Silva Augusto",
+		// 	email:"augustosilva5949@hotmail.com"
 
-		},
-		{
-			ayn: "Castro Chans Beatriz",
-			email:"beatriz.castrochans@comunidad.unne.edu.ar"
+		// },
+		// {
+		// 	ayn: "Castro Chans Beatriz",
+		// 	email:"beatriz.castrochans@comunidad.unne.edu.ar"
 
-		},
-		{
-			ayn: "Ana Maria Benitez",
-			email:"Benitezprofesorado@gmail.com"
+		// },
+		// {
+		// 	ayn: "Ana Maria Benitez",
+		// 	email:"Benitezprofesorado@gmail.com"
 
-		},
-		{
-			ayn: "Gómez Codutti Ana Elisa",
-			email:"anacodutti@live.com.ar"
+		// },
+		// {
+		// 	ayn: "Gómez Codutti Ana Elisa",
+		// 	email:"anacodutti@live.com.ar"
 
-		}
-		];
+		// }
+		// ];
 		
 		// this.coincidencia = function (nom) {
 		// 	console.log(nom);
 		// 	return nom.toLowerCase().search(this.filtro);
 		// };
+		getInscriptos();
+ 
+		function getInscriptos(){  
+			$http.get("../procesos/inscriptos.php").success(function(data){
+				
+				$scope.inscriptos = data;
+			});
+		}
 	});
 
 app.controller('TabsController', function () {
