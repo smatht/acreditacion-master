bf6e574060ba998586956346f61d43364351d341
Arcangel Andres <artigue_arcangel@hotmail.com>
2015-06-08 07:42:44 -0300

    se separaron un poco los tags html y se comento el codigo php embebido

--- administracion/certificados_view.php
+++ administracion/certificados_view.php
@@ -1,33 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="es"  ng-app="acreditacion">
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-		<script src="js/angular.min.js"></script>
-		<script src="js/app.js"></script>
-		<title>Certificados</title>
-	</head>
-	<body> 
-		<header>
-			<img src="images/teyet.png">
-		</header>
-		<div id="container">
-			<?php include_once("includes/menu.php"); ?>
-			<div ng-controller="AcreditacionCtr as acreCtrl">
-				<div id="buscar">
-					<label>Buscar</Label>
-					<input type="text" ng-model="acreCtrl.filtro" />
-				</div>
-				<table class="tabla_inscriptos">
-					<th>Apellido y Nombre</th><th>Accion</th>
-					<tr ng-repeat="insc in inscriptos | filter:acreCtrl.filtro">
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/angular.min.js"></script>
+	<script src="js/app.js"></script>
+	<title>Certificados</title>
+</head>
+<body> 
+	
+	<header>
+		<img src="images/teyet.png">
+	</header>
+	
+	<div id="container">
+		
+		<?php include_once("includes/menu.php"); ?>
+		<div ng-controller="AcreditacionCtr as acreCtrl">
+			
+			<div id="buscar">
+				<label>Buscar</Label>
+				<input type="text" ng-model="acreCtrl.filtro" />
+			</div>
+			
+			<table class="tabla_inscriptos">
+				<th>Apellido y Nombre</th><th>Accion</th>
+				<tr ng-repeat="insc in inscriptos | filter:acreCtrl.filtro">
 					<td>{{ insc.ayn }} ({{ insc.email }})</td>
 					<td><a target="_BLANK" href="../procesos/crear.php?nombre={{insc.ayn}}&correo={{insc.email}}">Ver</a></td>
+					
 					<!-- <td ng-if="!acreCtrl.coincidencia(insc.ayn)">{{insc.ayn}} ({{insc.email}})</td>
-					<td ng-if="!acreCtrl.coincidencia(insc.ayn)"><a target="_BLANK" href="../procesos/crear.php?nombre=<?php echo strtolower(str_replace(array(' ','/'),'_',$registro->ayn)); ?>&correo=<?php echo $registro->correo; ?>" >Ver</a></td> -->
-					</tr>
-				</table>
-			</div>
+					<td ng-if="!acreCtrl.coincidencia(insc.ayn)"><a target="_BLANK" href="../procesos/crear.php?nombre=<?php //echo strtolower(str_replace(array(' ','/'),'_',$registro->ayn)); ?>&correo=<?php //echo $registro->correo; ?>" >Ver</a></td> -->
+
+				</tr>
+			</table>
+		
 		</div>
-	</body>
+
+	</div>
+
+</body>
 </html>
