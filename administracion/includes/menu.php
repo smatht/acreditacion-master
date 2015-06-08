50325c69307967ba01f347f94dd6385fa630c4c5
unknown <mfalemany@gmail.com>
2015-06-07 23:18:47 -0300

    Se agrego la opcion Pagos en el menu

--- administracion/includes/menu.php
+++ administracion/includes/menu.php
@@ -3,4 +3,6 @@
 		Inscriptos</a></li>
 	<li><a href="certificados_view.php" id="<?php if(strstr($_SERVER['REQUEST_URI'], 'certificados_view.php')){echo 'activo';}?>">
 		Certificados</a></li>
+	<li><a href="pagos_view.php" id="<?php if(strstr($_SERVER['REQUEST_URI'], 'pagos_view.php')){echo 'activo';}?>">
+		Pagos</a></li>
 </ul>
