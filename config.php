2f17159792200de0330636618040380febc556c0
Facundo Rubin <facundorubin@gmail.com>
2015-06-08 19:47:18 -0300

    conf db

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
 
 	// config matias
-	$config_servidor_bd = '192.168.1.118:3306';
-	$config_usuario = 'slackware';
-	$config_clave = '123456';
+	//$config_servidor_bd = '192.168.1.118:3306';
+	//$config_usuario = 'slackware';
+	//$config_clave = '123456';
 
-	// $config_servidor_bd = 'ssh.facurubin.com.ar';
-	// $config_usuario = 'admin';
-	// $config_clave = 'mypass';
+	$config_servidor_bd = 'ssh.facurubin.com.ar';
+	$config_usuario = 'admin';
+	$config_clave = 'mypass';
 	$config_bd_name = 'teyet';
 ?>
